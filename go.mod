0d0937c85027e18f34c55f60b6ff3421065e2233
SouL-H <mhmtaydinw@gmail.com>
2022-06-23 19:23:01 +0300

    get userName

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module goRedis
 
 go 1.18
 
-require github.com/go-redis/redis v6.15.9+incompatible
+require (
+	github.com/99designs/gqlgen v0.17.10
+	github.com/go-redis/redis v6.15.9+incompatible
+)
 
 require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mitchellh/mapstructure v1.3.1 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,16 +7,25 @@ import (
 	"github.com/go-redis/redis"
 )
 
-func main(){
-	clinet := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
 		Password: "",
-		DB: 10,
-	})	
-	pong,err:=clinet.Ping().Result()//ping redis
-	if err !=nil{
+		DB:       10,
+	})
+	pong, err := client.Ping().Result() //ping redis
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(pong)
-	
+	err = client.Set("username", "user1", 0).Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Get Value
+	username, err := client.Get("username").Result()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(username, "is the username")
 }
